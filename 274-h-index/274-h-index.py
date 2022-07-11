e89152b73a32e3f6c55ebb224c294eb1267f97e9
asero13th <84772519+asero13th@users.noreply.github.com>
2022-07-11 13:17:22 -0700

    Time: 37 ms (94.88%), Space: 14.4 MB (31.61%) - LeetHub

---
+++ 274-h-index/274-h-index.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        N = len(citations)
+
+        buckets = [0] * (N + 1)
+
+        for elem in citations:
+            buckets[min(elem, N)] += 1
+
+        accum = list(accumulate(buckets[1:][::-1]))[::-1]
+
+        compar = [accum[i] >= i + 1 for i in range(N)]
+
+        return (compar + [0]).index(0)
