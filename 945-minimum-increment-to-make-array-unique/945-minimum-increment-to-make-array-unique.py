3f1d606f579381fea1771c8824ac81e9188225a7
asero13th <84772519+asero13th@users.noreply.github.com>
2022-09-11 21:03:16 -0700

    Time: 2762 ms (5.10%), Space: 32.7 MB (19.40%) - LeetHub

--- 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.py
+++ 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.py
@@ -3,8 +3,7 @@ class Solution:
         m = 0
         dup = []
         c = Counter(nums)
-        nums.sort()
-        for i in range(len(nums) + nums[-1]):
+        for i in range(len(nums) + max(nums)):
             if c[i] > 1:
                 dup.extend([i] * (c[i] -1))
 
