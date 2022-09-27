a1e0efe5df28c2a73c84e27dea59626804664392
asero13th <84772519+asero13th@users.noreply.github.com>
2022-09-27 04:28:58 -0700

    Time: 41 ms (73.32%), Space: 13.8 MB (57.50%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,17 +1,7 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        n = s.split(" ")
-        for i in range(0,len(n)):
-             for j in range(i + 1,len(n)):
-                     if n[j][len(n[j]) - 1] < n[i][len(n[i]) - 1]:
-                            tmp = n[i]
-                            n[i] = n[j]
-                            n[j] = tmp
-        s =  ' '.join(n)
-        n = []
-        for i in range(0,len(s) - 1):
-            if s[i].isdigit():
-                 continue
-            n.append(s[i])
-        s = ''.join(n)
-        return s
+        arr = s.split()
+        new_arr = [""] * len(arr)
+        for i in arr:
+            new_arr[int(i[-1]) - 1 ] = i[0:len(i) - 1]
+        return " ".join(new_arr)
