7ff692fc58006358e4f67237ddf6400e702cc5f1
asero13th <84772519+asero13th@users.noreply.github.com>
2022-10-04 05:36:09 -0700

    Time: 72 ms (8.89%), Space: 13.8 MB (72.26%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,20 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-            symbol_match = {
-                '}':'{',
-                ']':'[',
-                ')':'('
-            }
             stack = []
-            s = list(s)
-            for i in range(len(s)):
-                if s[i] in symbol_match.keys():
-                    if not stack:
-                        return False
-                    if symbol_match[s[i]] == stack[-1]:
+            bmap = {
+                
+                '(':')',
+                '{':'}',
+                '[':']'
+            }
+            
+            for i in s:
+                if i in bmap.keys():
+                    stack.append(i)
+                else:
+                    if stack and i == bmap[stack[-1]]:
                         stack.pop()
                     else:
                         return False
-                else:
-                    stack.append(s[i])
             return len(stack) == 0
+                
