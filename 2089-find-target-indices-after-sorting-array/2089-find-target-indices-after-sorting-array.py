314a114a4007cd53e6e0e21783cf5399049d272a
asero13th <84772519+asero13th@users.noreply.github.com>
2022-10-15 06:53:35 -0700

    Time: 102 ms (30.62%), Space: 13.9 MB (19.68%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -1,6 +1,5 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
-        arr = [i for i in range(len(nums)) if nums[i] == target]
-        return arr
-        
+        return [i for i in range(len(nums)) if nums[i] == target]
+       
