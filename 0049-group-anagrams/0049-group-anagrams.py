b2b911736b6da991cc2afe8964c533910f4a437c
asero13th <84772519+asero13th@users.noreply.github.com>
2022-10-31 06:31:36 -0700

    Time: 107 ms (91.69%), Space: 18.6 MB (35.04%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = []
+        tmp = ["".join(sorted(str)) for str in strs]
+        hashmap ={}
+        for idx,str in enumerate(tmp):
+            if not str in hashmap: hashmap[str] = [idx]
+            else: hashmap[str].append(idx)
+        print(hashmap)
+        for strarr in hashmap:
+            c = 0
+            tmp = []
+            while c < len(hashmap[strarr]):
+                tmp.append(strs[hashmap[strarr][c]])
+                c += 1
+            ans.append(tmp)
+        return ans
+        
+        
